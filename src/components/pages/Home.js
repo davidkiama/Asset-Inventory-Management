77f9f72072df0a31985f77e3baf5d5e07ea30a13
jacobrugano <jacobrugano.com>
2022-04-29 16:51:30 +0300

    Added the cards and stylings

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../App.css' /*Importing global css*/
 import HeroSection from '../HeroSection';  /**/
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
