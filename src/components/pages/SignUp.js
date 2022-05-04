ba4b43b85e178897eaa0d5ec39742aa5ba73cc61
davidkiama <davidkiama98@gmail.com>
2022-05-04 19:01:34 +0300

    Able to register, login & logout

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,61 +1,96 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { register } from "../../actions/auth";
+
 // import * as React from 'react'
 import "../../static/css/SignUp.css";
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="register-form-form-role">
-          <p className="register-form-form-role-title">Registering as? </p>
-          <div className="register-form-form-role-container">
-            <div className="register-form-form-role-user">
-              <input type="radio" name="role" id="role" />
-              <p>Manager</p>
-            </div>
-            <div className="register-form-form-role-mover">
-              <input type="radio" name="role" id="role" />
-              <p>Employee</p>
-            </div>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
-    );
-  }
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  password2: "",
+  manager: false,
+  employee: true,
+};
+
+function SignUp() {
+  const [userData, setUserData] = useState(initialState);
+  const dispatch = useDispatch();
+  const clearInputs = () => setUserData(initialState);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(register(userData));
+
+    clearInputs();
+
+    // Redirect to login page
+    navigate("/sign-in");
+  };
+
+  return (
+    <form style={{ width: "400px", margin: "20px auto" }} onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+
+      <div className="mb-3">
+        <label>Userame</label>
+        <input
+          type="text"
+          className="form-control"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          name="email"
+          placeholder="email"
+          onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="Enter password"
+          onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          name="password2"
+          className="form-control"
+          placeholder="Confirm password"
+          onChange={(e) => setUserData({ ...userData, [e.target.name]: e.target.value })}
+        />
+      </div>
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+      <p className="forgot-password text-right" style={{ margin: "20px auto" }}>
+        Already registered <a href="/sign-in">sign in?</a>
+      </p>
+    </form>
+  );
 }
+
+export default SignUp;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import token from "./auth";
+
+export default combineReducers({ token });
