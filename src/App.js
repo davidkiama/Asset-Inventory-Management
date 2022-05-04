735f2239c191aef1126cd4833ca756e841851123
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-05-04 14:04:26 +0300

    Reacjs Spring boot CRUD full stack application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// import logo from './logo.svg';
-import './App.css';
+// // import logo from './logo.svg';
+// import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Manager from './components/pages/Manager';
 import Employee from './components/pages/Employee';
 import SignUp from './components/pages/SignUp';
+import Login from './components/pages/Login'
 import "./App.css";
-
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/manager" element={<Manager/>} exact />
           <Route path="/employee" element={<Employee/>} exact />
           <Route path="/sign-up" element={<SignUp/>} exact />
+          <Route path="/sign-in" element={<Login />} />
         </Routes>
       </Router>
     </>
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,9 +1,61 @@
-import React from 'react';
-import '../../App.css';
-
-
-
-
-export default function SignUp() {
-    return <h5 className='sign-up'>Sign-Up Page....Mzee wa Asenali Andrew</h5>;
+import React, { Component } from "react";
+// import * as React from 'react'
+import "../../static/css/SignUp.css";
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="register-form-form-role">
+          <p className="register-form-form-role-title">Registering as? </p>
+          <div className="register-form-form-role-container">
+            <div className="register-form-form-role-user">
+              <input type="radio" name="role" id="role" />
+              <p>Manager</p>
+            </div>
+            <div className="register-form-form-role-mover">
+              <input type="radio" name="role" id="role" />
+              <p>Employee</p>
+            </div>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    );
+  }
 }
